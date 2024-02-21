c4228eccde0fb7db4d40c703e01aefec32027ea2
hotpepechka <kjhk3107@gmail.com>
2024-02-21 12:11:08 +0500

    добавил недостающий import

--- src/main/java/com/example/StaffCalc/dto/UserDTO.java
+++ src/main/java/com/example/StaffCalc/dto/UserDTO.java
@@ -1,11 +1,13 @@
 package com.example.StaffCalc.dto;
 
+import com.example.StaffCalc.models.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 
 @Getter
